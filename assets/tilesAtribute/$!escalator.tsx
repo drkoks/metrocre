1d51aaefd978dfa01a8adcf585ae286150d616f2
Youka4 <dim.youka4@gmail.com>
2024-04-09 14:28:00 +0300

    Graphics for tile map

---
+++ assets/tilesAtribute/$!escalator.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="$!escalator" tilewidth="192" tileheight="256" margin="64" tilecount="8" columns="2">
+ <image source="Downloads/KR Transportation Station - Subway and Train Tileset/tileset/$!escalator.png" width="576" height="1152"/>
+</tileset>
---
+++ assets/tilesAtribute/1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="1" tilewidth="32" tileheight="32" tilecount="540" columns="18">
+ <image source="Downloads/KR Transportation Station - Subway and Train Tileset/tileset/$!escalator_up_down.png" width="576" height="960"/>
+</tileset>
